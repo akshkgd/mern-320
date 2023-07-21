3e8ac974c04941c6f670a74deff13f8833c14ffb
Ashish Shukla <akshkgd@gmail.com>
2023-07-21 12:55:07 +0530

    wd

---
+++ intro to JS/app.js
@@ -0,0 +1,61 @@
+// variables
+// data types
+// arrays, objects
+// array of objects
+// es5 and es6
+
+
+// console.log('app.js is connected!!')
+// words and keywords in javascript
+// keywords => var, for function
+// words => ashish, codekaro
+// es5 and es6 are versions of javascript
+// es(ecma script)5 => 
+// es6 => new features are being added
+
+
+// variables => es5 => var, es6 =>  let and const 
+
+var coffee = 'brew';
+coffee = 'nescafe';
+// console.log(coffee);
+
+let address = 'allahabad';
+address = 'chitrakoot'
+// let age = 2;
+
+
+// console.log(address, age)
+// data types => strings, numbers, boolean, array,  objects
+
+// string => anything inside the "" or '' is string
+// numbers => store numbers (decimal values are also numbers)
+// boolean => store true or false
+let title = "codekaro";
+let age = 2.5;
+let isCompanyActive = true;
+
+// arrays and objects
+let shoppingList = ['bread', 'butter', 1, true]
+
+// console.log(shoppingList)
+// console.log(typeof shoppingList);
+
+
+// objects
+let user = {
+    name: 'ashish', 
+    email: 'ashish@gmail.com',
+    age: 24,
+    address: 'allahabad'
+}
+
+
+// console.log( user)
+
+
+let numbers = [1,2,3]
+numbers[3] = 4;
+numbers['name'] = 'ashish'
+
+console.log(numbers)
